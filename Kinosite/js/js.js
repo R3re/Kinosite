5868d0e523fa89ee246d854cf2e9cf85fe5a131d
R3re <KaJ2511001>
2019-08-18 17:11:52 +0500

    Сверстал хэдер. Добавил слайдер и поиск.

---
+++ Kinosite/js/js.js
@@ -0,0 +1,20 @@
+$(".header__search").click(function() {
+    $('.header__search-input').toggle();
+});
+$(document).on('click', function(e) {
+    if (!$(e.target).closest(".header-inner").length) {
+        $('.header__search-input').hide();
+    }
+    e.stopPropagation();
+});
+
+$(function(){
+    $('.header__slider').slick({
+        dots: true,
+        vertical: true,
+        verticalSwiping: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000
+    });
+});
